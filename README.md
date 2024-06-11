1da4a3a65e41386961041911e5bfcc3dfb679076
guan01 <guan01@gmail.com>
2024-06-10 21:06:35 -0400

    chapter01

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PythonDataStructures
+
+This tutorial is based on the book "Introduction to Python and Data Structures"
---
+++ chapter_01.py
@@ -0,0 +1,14 @@
+# guess what's returned from the following code?
+print(print(1))
+
+# guess what's returned from the following code?
+print(print(1 + 2))
+
+# round real numbers to the nearest even number
+print(round(1.5)) # 2
+print(round(2.5)) # 2
+print(round(3.5)) # 4
+print(round(4.5)) # 4
+print(round(5.5)) # 6
+print(round(6.5)) # 6
+
